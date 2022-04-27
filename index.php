08352f2e4a6880695df9490668496b33d60cfc11
manucarenjot <carenjotje@gmail.com>
2022-04-27 14:50:40 +0200

    Ajout

--- index.php
+++ index.php
@@ -12,6 +12,17 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Blog</title>
 </head>
+
+<div class="menu">
+    <nav>
+        <a href="?c=home">Acceuil</a>
+        <a href="?c=profil">Profil</a>
+        <a href="?c=login">Se connecter</a>
+        <a href="?c=register">S'inscrire</a>
+    </nav>
+
+</div>
+
 <?php
 $page = isset($_GET['c']) ? Routeur::secureUrl($_GET['c']) : 'home';
 $action = isset($_GET['a']) ? Routeur::secureUrl($_GET['a']) : 'index';
@@ -23,6 +34,12 @@ switch ($page) {
         break;
     case 'profil':
         Routeur::route('UserController', $action);
+        break;
+    case 'login':
+        Routeur::route('LoginController', $action);
+        break;
+    case 'register':
+        Routeur::route('RegisterController', $action);
         break;
     default:
         ErrorController::error404($page);
