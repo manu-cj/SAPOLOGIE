3b056e0c375a852c0de7d21f1760846b4b0ae0a9
manucarenjot <carenjotje@gmail.com>
2022-05-06 09:57:55 +0200

    fonction pour cacher des elements

--- View/user/profil.html.php
+++ View/user/profil.html.php
@@ -1,5 +1,2 @@
 <a href="?c=profil&a=update-profil&id=<?=$_SESSION['user']['id']?>">Modifier le profil</a>
 <a href="?c=profil&a=add-character&id=<?=$_SESSION['user']['id']?>">Ajouter un personnage</a>
-
-<h1 id="character">Les Personnages ⬇</h1>
-<h1 id="hide-character">Cacher</h1>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,44 @@
+function showElement($show, $hide, $hide2) {
+    $show.style.display = 'initial';
+    $hide.style.display = 'none';
+    $hide2.style.display = 'none';
+}
+
+function hideElement($show, $show2, $hide2) {
+    $show.style.display = 'none';
+    $show2.style.display = 'block';
+    $hide2.style.display = 'block';
+}
+
 let updateDescription = document.querySelector('#updateDescription');
+let classUpdateDescription = document.querySelector('.updateDescription');
+let classDescription = document.querySelector('.description');
+
 
 updateDescription.addEventListener("click", ()=> {
-    document.querySelector('.description').style.display = 'none';
-    document.querySelector('.updateDescription').style.display = 'inline';
-    updateDescription.style.display = 'none';
+    showElement(classUpdateDescription, classDescription, updateDescription);
 });
 
 let sendPicture = document.querySelector('#sendPicture');
 let hidden =  document.querySelector('#hidden');
+let addPicture = document.querySelector('#addPicture');
 sendPicture.addEventListener("click", ()=> {
-    document.querySelector('#addPicture').style.display = 'inline';
-    hidden.style.display = '';
-    sendPicture.style.display = 'none';
+    hideElement( sendPicture,hidden, addPicture);
 });
 
 hidden.addEventListener("click", ()=> {
-    document.querySelector('#addPicture').style.display = 'none';
-    hidden.style.display = 'none';
-    sendPicture.style.display = 'inline';
+    showElement(sendPicture, hidden, addPicture);
 });
 
 let character = document.querySelector('#character');
 let hideCharacter = document.querySelector('#hide-character');
-
+let allCharacters = document.querySelector('#characters');
 hideCharacter.addEventListener("click", ()=> {
-    character.style.display = '';
-    document.querySelector('#characters').style.display = 'none'
-    hideCharacter.style.display = 'none';
+    showElement(character, allCharacters, hideCharacter);
 })
 character.addEventListener("click", ()=> {
-    character.style.display = 'none';
-    document.querySelector('#characters').style.display = ''
-    hideCharacter.style.display = '';
+    hideElement(character, allCharacters, hideCharacter);
+
 })
 
-function hideElement($show, $hide, $hide2) {
-    $hide.style.display = 'none';
-    $show.style.display = '';
-    $hide2.style.display = '';
-}
+
