a5f708a005bb7ead37d6778c718cc6eeb33b95d3
manucarenjot <carenjotje@gmail.com>
2022-05-17 10:53:54 +0200

    Ajout d'une fonction mot de passe oublié

--- Model/Manager/Mail_validateManager.php
+++ Model/Manager/Mail_validateManager.php
@@ -16,8 +16,27 @@ class Mail_validateManager
         $insert->bindValue(':verification_mail', $pass);
 
         if ($insert->execute()) {
-            AbstractController::sendMail('$mail', 'Activer votre compte', 'Bienvenue sur notre Site,
-Pour activer votre compte, veuillez cliquer sur le lien ci dessous world-of-sapologie.com/?c=verification&mail='.$mail .'&code='.$pass, 'FROM: blog@world-of-sapologie.com');
+            $destinataire = $mail;
+            $sujet = 'Activer votre compte';
+            $message = "<div style='text-align: center; word-break: break-word;'>
+                            <h1>Bienvenue sur notre Site</h1>
+                            <p>Pour activer votre compte, veuillez cliquer sur le bouton ci dessous</p><br>
+
+                            <a href='world-of-sapologie.com/?c=verification&mail=$mail&code=$pass' style='padding: 15px; background-color: #00c0c0; color: white; font-size: 0.8em; border-radius: 10px; text-decoration-line: none'>Activer mon compte</a>
+                            <br>
+                            <br>
+                        </div>";
+
+            $header = "From: blog@world-of-sapologie.com";
+            $header .= 'Reply-To: blog@world-of-sapologie.com' . "\n";
+            $header .= 'Content-Type: text/html; charset="iso-8859-1"' . "\n";
+            $header .= 'Content-Transfer-Encoding: 8bit';
+            if (mail($destinataire, $sujet, $message, $header)) {
+                echo 'Votre message a bien été envoyé ';
+            } else // Non envoyé
+            {
+                echo "Votre message n'a pas pu être envoyé";
+            }
         }
 
     }
@@ -72,25 +91,67 @@ Pour activer votre compte, veuillez cliquer sur le lien ci dessous world-of-sapo
         }
     }
 
-    public static function updateCode($mail) {
-        $update =Connect::getPDO()->prepare("UPDATE aiu12_user SET validation_mail = :validation_mail WHERE mail = :mail");
+    public static function updateCode($mail)
+    {
+        $update = Connect::getPDO()->prepare("UPDATE aiu12_user SET validation_mail = :validation_mail WHERE mail = :mail");
         $update->bindValue(':mail', $mail);
 
         $pass = (new DateTime())->format('YmdHis') . uniqid();
 
         $update->bindValue(':validation_mail', $pass);
-       if ($update->execute()) {
-           AbstractController::sendMail('$mail', 'Activer votre compte', 'Pour activer votre compte, veuillez cliquer sur le lien ci dessous world-of-sapologie.com/?c=verification&mail='.$mail .'&code='.$pass, 'FROM: blog@world-of-sapologie.com');
-       }
+        if ($update->execute()) {
+            $destinataire = $mail;
+            $sujet = 'Activer votre compte';
+            $message = "<div style='text-align: center; word-break: break-word;'>
+                            <h1>Bienvenue sur notre Site</h1>
+                             <p>Pour activer votre compte, veuillez cliquer sur le bouton ci dessous</p><br>
+
+                              <a href='world-of-sapologie.com/?c=verification&mail=$mail&code=$pass' style='padding: 15px; background-color: #00c0c0; color: white; font-size: 0.9em; border-radius: 10px; text-decoration-line: none'>Activer mon compte</a>
+                              <br>
+                              <br>
+                        </div>";
+
+            $header = "From: blog@world-of-sapologie.com";
+            $header .= 'Reply-To: blog@world-of-sapologie.com' . "\n";
+            $header .= 'Content-Type: text/html; charset="iso-8859-1"' . "\n";
+            $header .= 'Content-Transfer-Encoding: 8bit';
+            if (mail($destinataire, $sujet, $message, $header)) {
+                echo 'Votre message a bien été envoyé ';
+            } else // Non envoyé
+            {
+                echo "Votre message n'a pas pu être envoyé";
+            }
+        }
     }
 
-    public static function ForgotPassword($mail, $forgotPassword) {
+    public static function ForgotPassword($mail, $forgotPassword)
+    {
         $insert = Connect::getPDO()->prepare("UPDATE aiu12_user SET forgot_password_code = :forgot_password_code WHERE mail = :mail");
         $insert->bindValue('mail', $mail);
         $insert->bindValue(':forgot_password_code', $forgotPassword);
         if ($insert->execute()) {
-            mail('$mail', 'Activer votre compte', 'Bienvenue sur notre Site,
-Pour activer votre compte, veuillez cliquer sur le lien ci dessous world-of-sapologie.com/?c=verification&mail='.$mail .'&code='.$forgotPassword, 'FROM: blog@world-of-sapologie.com');
+            $destinataire = $mail;
+            $sujet = 'réinisialiser le mot de passe';
+
+            $message = "<div style='text-align: center; word-break: break-word;'>
+                            <h1>Bienvenue sur notre Site</h1>
+                            <p>Pour réinitialiser votre mot de passe, veuillez cliquer sur le bouton ci dessous</p><br>
+
+                            <a href='world-of-sapologie.com/?c=forgot-password&mail=$mail&code=$forgotPassword' style='padding: 15px; background-color: #00c0c0; color: white; font-size: 0.9em; border-radius: 10px; text-decoration-line: none'>Réinitialiser le mot de passe</a>
+                            <br>
+                            <br>
+                        </div>";
+
+            $header = "From: blog@world-of-sapologie.com";
+            $header .= 'Reply-To: blog@world-of-sapologie.com' . "\n";
+            $header .= 'Content-Type: text/html; charset="iso-8859-1"' . "\n";
+            $header .= 'Content-Transfer-Encoding: 8bit';
+            if (mail($destinataire, $sujet, $message, $header)) {
+                echo 'Votre message a bien été envoyé ';
+            } else // Non envoyé
+            {
+                echo "Votre message n'a pas pu être envoyé";
+            }
             $alert = [];
             $alert[] = '<div class="alert-succes">Un mail de vérification vous a été envoyé</div>';
             if (count($alert) > 0) {
@@ -101,7 +162,8 @@ Pour activer votre compte, veuillez cliquer sur le lien ci dessous world-of-sapo
         }
     }
 
-    public static function verifCodeForgotPassword($mail, $code) {
+    public static function verifCodeForgotPassword($mail, $code)
+    {
         $select = Connect::getPDO()->prepare("SELECT * FROM aiu12_user WHERE mail = :mail");
         $select->bindValue(':mail', $mail);
 
@@ -111,8 +173,7 @@ Pour activer votre compte, veuillez cliquer sur le lien ci dessous world-of-sapo
                 if ($code === $data['forgot_password_code']) {
                     $_SESSION['mail'] = $mail;
                     header('LOCATION: ?c=reset-password');
-                }
-                else {
+                } else {
                     $alert = [];
                     $alert[] = '<div class="alert-error">Ce lien n\'est pas valide</div>';
                     if (count($alert) > 0) {
